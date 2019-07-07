07a3d7cceba1f586ecbcaf331c1b510e789a5e7a
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-07-07 23:10:52 +0200

    chore(build): fix build

--- docs/.vuepress/components/Topbar.vue
+++ docs/.vuepress/components/Topbar.vue
@@ -1,61 +1,63 @@
 <template>
     <n-div flex right-center>
-        <template v-if="$viewport.isTabletDown">
-            <router-link to="/" tag="h1">NoahUI</router-link>
+        <ClientOnly>
+            <template v-if="$viewport.isTabletDown">
+                <router-link to="/" tag="h1">NoahUI</router-link>
 
-            <n-div sm:narrow>
-                <n-button
-                    circle
-                    ghost
-                    white
-                    xlarge
-                    :icon="showDrawer ? 'fas fa-times' : 'fas fa-bars'"
-                    @click="showDrawer = !showDrawer"
-                />
-            </n-div>
+                <n-div sm:narrow>
+                    <n-button
+                        circle
+                        ghost
+                        white
+                        xlarge
+                        :icon="showDrawer ? 'fas fa-times' : 'fas fa-bars'"
+                        @click="showDrawer = !showDrawer"
+                    />
+                </n-div>
 
-            <n-drawer
-                attachment="right"
-                v-model="showDrawer"
-            >
-                <n-div padding="medium">
+                <n-drawer
+                    attachment="right"
+                    v-model="showDrawer"
+                >
+                    <n-div padding="medium">
+                        <n-button
+                            outlined
+                            primary
+                            href="https://github.com/arckinteractive/noah-ui"
+                            target="_blank"
+                            text="Fork & Go"
+                            icon="fab fa-github"
+                            style="width: 100%"
+                        ></n-button>
+                    </n-div>
+
+                    <hr/>
+
+                    <n-div padding="medium">
+                        <SearchBox style="width: 100%"/>
+                    </n-div>
+
+                    <hr/>
+
+                    <Sidebar/>
+                </n-drawer>
+            </template>
+
+            <template v-else>
+                <n-div flex right-center>
                     <n-button
                         outlined
-                        primary
+                        white
                         href="https://github.com/arckinteractive/noah-ui"
                         target="_blank"
                         text="Fork & Go"
                         icon="fab fa-github"
-                        style="width: 100%"
                     ></n-button>
+
+                    <SearchBox/>
                 </n-div>
-
-                <hr />
-
-                <n-div padding="medium">
-                    <SearchBox style="width: 100%"/>
-                </n-div>
-
-                <hr />
-
-                <Sidebar/>
-            </n-drawer>
-        </template>
-
-        <template v-else>
-            <n-div flex right-center>
-                <n-button
-                    outlined
-                    white
-                    href="https://github.com/arckinteractive/noah-ui"
-                    target="_blank"
-                    text="Fork & Go"
-                    icon="fab fa-github"
-                ></n-button>
-
-                <SearchBox/>
-            </n-div>
-        </template>
+            </template>
+        </ClientOnly>
     </n-div>
 </template>
 
