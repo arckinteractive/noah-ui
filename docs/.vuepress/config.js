65679cd6f2022e6282b4ffbe302d74c549ad4b06
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-07-07 10:35:04 +0200

    chore(docs): add PWA plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
     title: 'Noah UI',
     description: 'Vue Framework for Apocalyptic Schedules',
     base: process.env.BUILD_BASE || '/',
+    plugins: [
+        ['@vuepress/pwa'],
+    ],
 
     //If you are running into difficulties debugging code, uncomment this
     chainWebpack: (config, isServer) => {
