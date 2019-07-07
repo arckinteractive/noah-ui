9186ace67153fafb3124fd0401feb23db97b8c40
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-07-07 18:27:36 +0200

    chore(docs): update icon docs

---
+++ docs/.vuepress/components/demos/Icons.vue
@@ -0,0 +1,30 @@
+<template>
+    <n-div flex stacked>
+        <n-div flex>
+            <n-icon v-for="icon in icons" :key="icon" :icon="icon"></n-icon>
+        </n-div>
+
+        <n-div flex>
+            <n-icon v-for="icon in icons" :key="icon" :icon="icon" text-color="primary"></n-icon>
+        </n-div>
+
+        <n-div flex>
+            <n-icon v-for="icon in icons" :key="icon" :icon="icon" text-size="large"></n-icon>
+        </n-div>
+    </n-div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            icons: [
+                'fas fa-users',
+                'fab fa-github',
+                'far fa-envelope',
+            ],
+        };
+    },
+};
+</script>
+
