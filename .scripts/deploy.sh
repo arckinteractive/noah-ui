ac76a1751dc627ba447128cbded75bad535618a1
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-07-11 09:16:45 +0200

    chore(releases): bump version

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:arckinteractive/noah-ui.git master:gh-pages
+git push -f https://github.com/arckinteractive/noah-ui.git master:gh-pages
 
 cd -
